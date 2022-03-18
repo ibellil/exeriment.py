2f8246608e6b5b1b7d75eb72516f9a018e9d6e6f
ibellil <100691724+ibellil@users.noreply.github.com>
2022-03-18 16:18:57 +0100

    Update tt.py

--- tt.py
+++ tt.py
@@ -19,4 +19,48 @@ selectbox = st.selectbox(
     ["Yes", "No"]
 )
 st.write(f"You selected {selectbox}")
+st.title("Welcome to Streamlit! Checkbox")
 
+checkbox_one = st.checkbox("Yes")
+checkbox_two = st.checkbox("No")
+
+if checkbox_one:
+    value = "Yes"
+elif checkbox_two:
+    value = "No"
+else:
+    value = "No value selected"
+
+st.write(f"You selected: {value}")
+
+#Line chart in streamlit
+import numpy as np
+st.title("Welcome to Streamlit! Line chart")
+st.write("Line Chart in Streamlit")
+# 10 * 2 dimensional data
+chart_data = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=[f"Col{i+1}" for i in range(2)]
+)
+
+st.line_chart(chart_data)
+#Plotly charts in streamlit
+#pip install plotly
+import plotly.graph_objects as go
+
+st.title("Welcome to Streamlit! Plotly Graphs")
+
+fig = go.Figure(
+    data=[go.Pie(
+        labels=['A', 'B', 'C'],
+        values=[30, 20, 50]
+    )]
+)
+fig = fig.update_traces(
+    hoverinfo='label+percent',
+    textinfo='value',
+    textfont_size=15
+)
+
+st.write("Pie chart in Streamlit")
+st.plotly_chart(fig)
