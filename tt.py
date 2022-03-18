17cfd82eae99526e1f1fd949aa8703dd542d340e
ibellil <100691724+ibellil@users.noreply.github.com>
2022-03-18 16:00:30 +0100

    Update tt.py

--- tt.py
+++ tt.py
@@ -3,3 +3,14 @@ import streamlit as st
 #Widgets
 value = st.slider('val')  # this is a widget
 st.write(value, 'squared is', value * value)
+import pandas as pd
+st.title("Welcome to Streamlit! DataFrame")
+st.write("Our first DataFrame")
+st.write(
+  pd.DataFrame({
+      'A': [1, 2, 3, 4],
+      'B': [5, 6, 7, 8]
+    })
+)
+
+
